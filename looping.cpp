eae7ff46daabe4ce3f73a82bc89118a4a49fd16f
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-03-30 17:12:56 +0700

    Menambah perulangan FOR

--- looping.cpp
+++ looping.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
+
+int main(){
+    int x;
+
+    //Perulangan dengan FOR
+    //mencetak tulisan sebanyak 5 kali
+    cout<<"PERULANGAN FOR"<<endl;
+
+    for(x = 1; x <= 5; x++){
+        cout << x << ". Belajar Perulangan C++" << endl;
+    }
+
+    return 0;
+}
